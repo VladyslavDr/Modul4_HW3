43be305a9fd97b028e866851721d1bbd14137781
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-11-14 19:59:39 +0200

    Add entities

---
+++ Modul4Task/Modul4Task/Entities/Emloyee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4Task
+{
+    public class Emloyee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HiredData { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public int OfficeId { get; set; }
+        public int TitleId { get; set; }
+    }
+}
